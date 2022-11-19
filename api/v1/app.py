1b1d725d75089f34874e9c2f9511e4571521a02c
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-19 21:57:46 +0100

    Task 3

--- api/v1/app.py
+++ api/v1/app.py
@@ -16,4 +16,6 @@ def teardown_appcontext(exception):
 
 
 if __name__ == "__main__":
-	# getenv
+	host = getenv("HBNB_API_HOST", default="0.0.0.0")
+	port = getenv("HBNB_API_PORT", default=5000)
+	app.run(host=host, port=int(port), threaded=True, debug=True)
