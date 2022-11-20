2c4cdef108d3b298a46a7aff998fc64118c4c9de
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-20 13:41:23 +0100

    Task 5

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module is the entry point for flask api"""
-from flask import Flask, jsonify
+from flask import Flask, jsonify, make_response
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -19,7 +19,7 @@ def teardown_appcontext(exception):
 @app.errorhandler(404)
 def not_found(exception):
     """404 not found handler"""
-    return jsonify({"error": "Not found"})
+    return make_response(jsonify({"error": "Not found"}), 404)
 
 
 if __name__ == "__main__":
