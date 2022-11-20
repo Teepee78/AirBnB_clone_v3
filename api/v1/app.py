6c13df603ebaa40ab775deff89cad6cf7c52d421
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-20 13:33:40 +0100

    Task 4

--- api/v1/app.py
+++ api/v1/app.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 """This module is the entry point for flask api"""
-from flask import Flask
+from flask import Flask, jsonify
 from models import storage
 from api.v1.views import app_views
 from os import getenv
@@ -14,6 +14,12 @@ app.register_blueprint(app_views, url_prefix="/api/v1")
 def teardown_appcontext(exception):
     """Tear down the app context"""
     storage.close()
+
+
+@app.errorhandler(404)
+def not_found(exception):
+	"""404 not found handler"""
+	return jsonify({"error": "Not found"})
 
 
 if __name__ == "__main__":
