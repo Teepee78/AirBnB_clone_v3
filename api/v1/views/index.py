ba9eb89185791480023358218020db9d1385a3eb
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-20 11:24:08 +0100

    Update

--- api/v1/views/index.py
+++ api/v1/views/index.py
@@ -1,8 +1,10 @@
 #!/usr/bin/python3
+"""This module defines blueprints"""
 from api.v1.views import app_views
 from flask import jsonify
 
 
 @app_views.route('/status')
 def status():
+    """Returns status information"""
     return jsonify({"status": "OK"})
