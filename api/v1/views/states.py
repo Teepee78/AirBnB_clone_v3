eb010e01be66c8b317b8c60749741914e7ffd75f
Toluwalase Phillips <teepee@Toluwalases-MacBook-Pro.local>
2022-11-20 16:29:14 +0100

    Task 6

--- api/v1/views/states.py
+++ api/v1/views/states.py
@@ -1,13 +1,14 @@
 #!/usr/bin/python3
 """This module defines views for state object"""
-from flask import jsonify, make_response, request, abort
+from flask import abort, jsonify, make_response, request
 
 from api.v1.views import app_views
 from models import storage
 from models.state import State
 
 
-@app_views.route('/states', methods=['GET', 'POST'])
+@app_views.route('/states',
+	methods=['GET', 'POST'], strict_slashes=False)
 def states():
 	"""Retrieves the list of all State objects"""
 	if request.method == 'GET':
@@ -31,7 +32,8 @@ def states():
 		abort(400, jsonify({"error": "Missing name"}))
 
 
-@app_views.route('states/<uuid:state_id>', methods=['GET', 'DELETE', 'PUT'])
+@app_views.route('states/<uuid:state_id>',
+	methods=['GET', 'DELETE', 'PUT'], strict_slashes=False)
 def states_id(state_id):
 	"""Retrieves a State object by its id"""
 	state = storage.get(State, state_id)
@@ -46,8 +48,9 @@ def states_id(state_id):
 				abort(400, jsonify({"error": "Not a JSON"}))
 			details = request.get_json()
 			forbidden = ["id", "created_at", "updated_at"]
-			for k, v in details:
+			for k, v in details.items():
 				if k not in forbidden:
 					setattr(state, k, v)
-			return jsonify(state.to_dict())
+			state.save()
+			return make_response(jsonify(state.to_dict()), 200)
 	abort(404, jsonify({"error": "Not found"}))
