70c55b6847fffb7b248ce5cb6b59c40512991487
codestronomer <johnrumide6@gmail.com>
2022-11-21 03:56:07 +0100

    [fix]

--- api/v1/views/__init__.py
+++ api/v1/views/__init__.py
@@ -1,4 +1,8 @@
 #!/usr/bin/python3
+from flask import Blueprint
+
+app_views = Blueprint('app_views', __name__)
+
 from api.v1.views.index import *
 from api.v1.views.states import *
 from api.v1.views.cities import *
@@ -6,6 +10,3 @@ from api.v1.views.amenities import *
 from api.v1.views.users import *
 from api.v1.views.places import *
 from api.v1.views.places_reviews import *
-from flask import Blueprint
-
-app_views = Blueprint('app_views', __name__)
